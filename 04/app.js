374f43e18ad15172cf09dac290daf6ac6dada2df
YURI <reddresseson@gmail.com>
2018-09-21 16:57:08 +0900

    체스판 html에 포함시키기

--- 04/app.js
+++ 04/app.js
@@ -1,16 +1,26 @@
 var wrap = document.querySelector('.wrap');
 var str = "";
 
-for(var i=0; i<4; i++){
-    if(i%2){
-        str += "<div class='b'></div><div class='w'></div><div class='b'></div><div class='w'></div>"
-    }
-    else{
-        str += "<div class='w'></div><div class='b'></div><div class='w'></div><div class='b'></div>"
-    }
-}
+//ver1
+// for(var i=0; i<2; i++){
+//     str += "<div class='b'></div><div class='w'> </div><div class='b'></div><div class='w'>  </div><div class='w'></div><div class='b'> </div><div class='w'></div><div class='b'></div>"
+// }
+// wrap.innerHTML = str; //DOM 접근은 횟수가 적을 수록 좋음.
 
+
+//ver2
+var bfirst = "<div class='b'></div><div class='w'></div>"
+var wfirst = "<div class='w'></div><div class='b'></div>"
+
+for(var i=0; i<4; i++)
+{
+    if(i%2===0)
+        str += bfirst+bfirst;
+    else
+        str += wfirst+wfirst;
+}
 wrap.innerHTML = str;
+
 
 // var board = document.getElementById("board");
 //     for(var i=0; i<4; i++){
