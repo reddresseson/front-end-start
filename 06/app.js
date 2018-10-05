c990988468604d65ad131c539e0d55a62a26137e
YURI <reddresseson@gmail.com>
2018-10-05 16:58:52 +0900

    header를 fetch의 인자로 보내기

--- 06/app.js
+++ 06/app.js
@@ -4,7 +4,13 @@ var appkey = '5e306eca1d4dd7e798167f5c11a8276c';
 var query = '제주대학교';
 var url = `https://dapi.kakao.com/v2/search/web?query=${query}`;
 
-fetch(url)
+var myHeaders = new Headers();
+myHeaders.append('Authorization', 'KakaoAK 5e306eca1d4dd7e798167f5c11a8276c');
+var options = {
+    headers: myHeaders
+};
+
+fetch(url, options)
   .then(function(response){
     response.json().then(function(data){
       console.log('json data:',data);
